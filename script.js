92abb97f936490eb99be4590a9651d2a15a32b95
Abdullah Mamazhusup <16565377+kyrgyzweb@users.noreply.github.com>
2026-06-13 16:20:52 +0600

    Create Human choice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. Writing logic tot the Computer choice
+// 1. Write logic tot the Computer choice
 const getComputerChoice = () => {
     let randomNum = Math.floor(Math.random() * 3);
 
@@ -12,5 +12,10 @@ const getComputerChoice = () => {
 }
 
 // 2. Write logic to the Human choice
+const getHumanChoice = () => {
+    let humanChoice = prompt("Enter your choice: ");
+    return humanChoice;
+}
+
 
 
