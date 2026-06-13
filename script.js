3b307c9032f27ef957ffa0bfd7187a6a205c1548
Abdullah Mamazhusup <16565377+kyrgyzweb@users.noreply.github.com>
2026-06-13 16:09:34 +0600

    Write logic to the Computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Bismillah!')
+// 1. Writing logic tot the Computer choice
+const getComputerChoice = () => {
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if(randomNum === 0) {
+        return 'rock';
+    } else if(randomNum === 1) {
+        return 'paper';
+    } else {
+        return 'scissor';
+    }
+}
+
+// 2. Write logic to the Human choice
+
+
